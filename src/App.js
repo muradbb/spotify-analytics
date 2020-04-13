81f71b5f8bcf4779b3ee9030ef66ffc5a0118c6b
ipl4dc <ipl4dc@inf.elte.hu>
2020-04-13 22:57:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let appStyle={
+  bgcolor:'#2F3133'
+}
+
+function Introduction(){
+  return(
+    <div>
+      <h1>You have "N" playlists which is a total of "N" hours</h1>
+    </div>
+  )
+}
+
+function FavArtists(){
+  return(
+    <div>
+      <img/>
+      <h3>Artist name</h3>
+      <ul>
+        <li>SongName</li>
+        <li>SongName</li>
+        <li>SongName</li>
+      </ul>
+    </div>
+  )
+}
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={appStyle}>
+      <header>
+      <Introduction/>
       </header>
+      <FavArtists/>
     </div>
   );
 }
